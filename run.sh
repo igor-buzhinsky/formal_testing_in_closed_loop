4eb9bf8c832f38bb740abd22d91c66cc9caf5db6
Igor Buzhinsky <igor.buzhinsky@gmail.com>
2018-03-17 10:55:20 +0200

    Minor changes.

--- run.sh
+++ run.sh
@@ -23,13 +23,11 @@ comparison() {
         echo "==== comparison with $i floors, basedir=$basedir ===="
         
         # Framework: synthesis
-        call_nusmv synthesize-coverage-tests --maxlen $maxlen --includeInternal --output test-small.bin $finite --minimize --coi > log
-        print_log
+        call_nusmv synthesize-coverage-tests --maxlen $maxlen --includeInternal --output test-small.bin $finite --minimize --coi > log; print_log
         print_test_suite test-small.bin > /dev/null
         
         # Framework: execution
-        call_spin run --input test-small.bin --verify --output out-small.pml $finite --panO 0 > log
-        print_log
+        call_spin run --input test-small.bin --verify --output out-small.pml $finite --panO 0 > log; print_log
         
         if [[ "$nomc" == true ]]; then
             continue
@@ -41,28 +39,26 @@ comparison() {
         
         # BDD-based LTL MC 
         nusmv_spec_file=spec-ltl.smv
-        call_nusmv closed-loop-verify --verbose --dynamic --coi > log
+        call_nusmv closed-loop-verify --verbose --dynamic --coi > log; print_log
         nusmv_spec_file=spec.smv
-        print_log
         
         # BDD-based CTL MC
-        call_nusmv closed-loop-verify --verbose --dynamic --coi > log
-        print_log
+        call_nusmv closed-loop-verify --verbose --dynamic --coi > log; print_log
     done
 }
 
 additional_tests() {
     set_floors 2
 
-    call_spin closed-loop-verify 
-    call_nusmv closed-loop-verify 
-    call_spin generate-random --number 2 --length 2 --output test.bin $seed
-    call_spin run --input test.bin --verify --output out.pml $finite --panO 0
-    #check_spin "set_floors 2" # takes long
-    #check_nusmv "set_floors 2" # FIXME hangs after test generation...
+    #call_spin closed-loop-verify 
+    #call_nusmv closed-loop-verify 
+    #call_spin generate-random --number 2 --length 2 --output test.bin $seed
+    #call_spin run --input test.bin --verify --output out.pml $finite --panO 0
+    check_spin "set_floors 2" # takes long
+    #check_nusmv "set_floors 2" # 16.03 passed
 }
 
-comparison 2 2 true
+comparison 2 2 false
 #comparison 3 15
 #additional_tests
 
